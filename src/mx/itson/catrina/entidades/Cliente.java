b0452925208ed1adeb20c9a4066b8694a6bdd721
Ax3lVM <axeltadeovm1902@outlook.com>
2022-10-30 23:17:58 -0700

    Primera versión

---
+++ src/mx/itson/catrina/entidades/Cliente.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.catrina.entidades;
+
+/**
+ *
+ * @author axelt
+ */
+public class Cliente {
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the domicilio
+     */
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    /**
+     * @param domicilio the domicilio to set
+     */
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    /**
+     * @return the rfc
+     */
+    public String getRfc() {
+        return rfc;
+    }
+
+    /**
+     * @param rfc the rfc to set
+     */
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    /**
+     * @return the cp
+     */
+    public int getCp() {
+        return cp;
+    }
+
+    /**
+     * @param cp the cp to set
+     */
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+    private String codigo;
+    private String nombre;
+    private String domicilio;
+    private String rfc;
+    private int cp;
+}
---
+++ src/mx/itson/catrina/entidades/Movimiento.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.catrina.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author axelt
+ */
+public class Movimiento {
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public Double getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(Double cantidad) {
+        this.cantidad = cantidad;
+    }
+    private Date fecha;
+    private String descripcion;
+    private Double cantidad;
+}
